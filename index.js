2fd2bc03be75817a97afd8533f0424589fd19e49
Lorenzo Sicilia <lorenzo.sicilia@outlierventures.io>
2023-11-06 14:03:42 +0000

    Integrate the action with the upload function

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 import { getInput, setOutput, setFailed } from '@actions/core'
-import {context} from '@actions/github'
+import { context } from '@actions/github'
+import fs from 'fs'
+import { upload } from './src/upload.js'
 
 try {
-    const directory = getInput('directory');
-    console.log(`Hello ${directory}!`);
-    const time = (new Date()).toTimeString();
-    setOutput("txId", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+  const directory = getInput('directory')
+  const dryRun = getInput('dry-run')
+  const base64PrivateKey = getInput('private-key')
+  console.log(`Hello ${directory}!`)
+  const time = new Date().toTimeString()
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+
+  let privateKey
+  try {
+    privateKey = JSON.parse(
+      Buffer.from(base64PrivateKey, 'base64').toString('utf-8'),
+    )
+  } catch (e) {
+    throw new Error(
+      `Missing private key or not encoded as base64: ${e.message}`,
+    )
+  }
+
+  const transactionConfirmedData = await upload(directory, privateKey, dryRun)
+  return transactionConfirmedData.block_indep_hash
 } catch (error) {
-    setFailed(error.message);
+  setFailed(error.message)
 }
