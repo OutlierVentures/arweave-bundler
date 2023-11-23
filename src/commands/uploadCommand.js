e3b9832e7d7a1412f2fcd6a92b21308bb3bf5cdb
Lorenzo Sicilia <lorenzo.sicilia@outlierventures.io>
2023-11-23 11:09:31 +0000

    fix: parse the dryRun argument and convert to a boolean

--- src/commands/uploadCommand.js
+++ src/commands/uploadCommand.js
@@ -1,35 +1,37 @@
 import { upload } from '../upload.js'
-import {parsePrivateKey} from '../utils/parsePrivateKey.js'
+import { parsePrivateKey } from '../utils/parsePrivateKey.js'
 
 export const command = 'upload <directory>'
 export const description = 'Upload a directory to Arweave as bundle'
 
 export const builder = (yargs) => {
-  yargs.positional('directory', {
-    describe: 'The directory containing files to upload',
-    type: 'string',
-  })
-  .option('private-key', {
-    describe:
-      'Path to the private key file (default: wallet.json) or base64 encoded private key (JWK)',
-    type: 'string',
-  })
-  .option('dry-run', {
-    describe: 'run everything expect submitting the transaction',
-    type: 'boolean',
-    default: false,
-  })
+  yargs
+    .positional('directory', {
+      describe: 'The directory containing files to upload',
+      type: 'string',
+    })
+    .option('private-key', {
+      describe:
+        'Path to the private key file (default: wallet.json) or base64 encoded private key (JWK)',
+      type: 'string',
+    })
+    .option('dry-run', {
+      describe: 'run everything expect submitting the transaction',
+      type: 'boolean',
+      default: false,
+    })
 }
 
 export const handler = async (argv) => {
-  const { directory, dryRun } = argv
+  const { directory } = argv
   const privateKey = parsePrivateKey(argv)
+  const dryRun = (argv.dryRun || 'false').toLowerCase() === 'true'
   return await upload(directory, privateKey, dryRun)
 }
 
 export default {
-    command,
-    description,
-    builder,
-    handler
+  command,
+  description,
+  builder,
+  handler,
 }
