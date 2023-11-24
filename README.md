20199ee01afd5ada1fda0836e47e0283e8bb8f03
Lorenzo Sicilia <lorenzo.sicilia@outlierventures.io>
2023-11-24 14:46:56 +0000

    chore: refactoring and update to the latest version

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ The lib use semantic versioning
 ## Use the action
 
 ```
-uses: outlierventures/arweave-bundler-action@v0.2.0
+uses: outlierventures/arweave-bundler-action@v0.2.4
 with:
   directory: build/
   private-key: ${secret.ARWEAVE_PRIVATE_KEY}
--- src/utils/getContentTypeByExtension.js
+++ src/utils/getContentTypeByExtension.js
@@ -1,29 +1,21 @@
+const CONTENT_TYPE_MAPPING = {
+  '.html': 'text/html',
+  '.txt': 'text/plain',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.js.map': 'application/json',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+}
+
 export function getContentTypeByExtension(ext) {
-  switch (ext.toLowerCase()) {
-    case '.html':
-      return 'text/html'
-    case '.txt':
-      return 'text/plain'
-    case '.css':
-      return 'text/css'
-    case '.js':
-      return 'application/javascript'
-    case '.js.map':
-      return 'application/json'
-    case '.json':
-      return 'application/json'
-    case '.png':
-      return 'image/png'
-    case '.ico':
-      return 'image/x-icon'
-    case '.jpg':
-    case '.jpeg':
-      return 'image/jpeg'
-    case '.gif':
-      return 'image/gif'
-    case '.svg':
-      return 'image/svg+xml'
-    default:
-      return 'application/octet-stream' // default to binary data
-  }
+  const extension = ext.toLowerCase()
+  return CONTENT_TYPE_MAPPING[extension]
+    ? CONTENT_TYPE_MAPPING[extension]
+    : 'application/octet-stream'
 }
