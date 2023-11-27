42098e036e518f2aaea676fb9b3bf0605da17f04
Lorenzo Sicilia <lorenzo.sicilia@outlierventures.io>
2023-11-27 19:07:51 +0000

    chore: handle set command

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import { getInput, setOutput, setFailed } from '@actions/core'
+import { setRecord } from './src/setRecord.js'
 import { upload } from './src/upload.js'
 import { getAddress } from './src/utils/info.js'
-import {parsePrivateKey} from './src/utils/parsePrivateKey.js'
+import { parsePrivateKey } from './src/utils/parsePrivateKey.js'
 
 try {
   const command = getInput('command')
   const directory = getInput('directory')
   const dryRun = getInput('dry-run')
   const base64PrivateKey = getInput('private-key')
+  const antAddress = getInput('ant-address')
+  const manifestId = getInput('manifest-id')
 
   const privateKey = parsePrivateKey(base64PrivateKey)
 
@@ -18,6 +21,9 @@ try {
     case 'address':
       console.log('address:', getAddress(privateKey))
       setOutput('address', getAddress(privateKey))
+      break
+    case 'set':
+      await setRecord(antAddress, manifestId, privateKey,dryRun)
       break
     default:
       throw new Error(`${command} not supported`)
