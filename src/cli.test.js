2dfd61837c39bdbd29f813cf24351e89b17c778a
Scott-Canning <34758484+Scott-Canning@users.noreply.github.com>
2023-11-28 11:19:14 -0500

    fix: implemented set command and unit tests using arweave.js

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,9 +1,9 @@
 import assert from 'node:assert';
-import { it, describe, mock, beforeEach } from 'node:test'
+import { it, describe } from 'node:test'
 import { execSync } from 'child_process';
 import privateKeyMock from './fixtures/wallet.json' assert { type: 'json' }
 
-describe.only('CLI', () => {
+describe('CLI', () => {
   const directory = 'src/fixtures/test-dir';
 
   it('uploads a directory to Arweave as dry-run', () => {
@@ -18,17 +18,14 @@ describe.only('CLI', () => {
     assert.ok(output.trim().includes(`Starting upload of '${directory}'`), 'Expected output or empty string if no output');
   });
 
-  it.only('set an ArNS record as dry run', () => {
+  it('set an ArNS record as dry run', () => {
 
     const privateKey = btoa(JSON.stringify(privateKeyMock))
     const command = `src/cli.js set --ant-address vyti5Zsqbk25hTsYrIkePpwKK7jdiXNDHRKa09die3g --manifest-id VE_LjHiqLA2Je2pCc5CP4irvxPVab4mGIViSOv2EspA --private-key ${privateKey} --dry-run`
 
     // Execute the CLI command using execSync
     const output = execSync(command).toString();
-
-    // Assert the output or other expectations based on the script behavior
-    //TODO LS figure out a better way to have a dry run with less requirements
-    assert.ok(output.trim().includes(`Caller is not the token owner!`));
+    assert.ok(output.trim().includes('Setting Arweave Name Token record to VE_LjHiqLA2Je2pCc5CP4irvxPVab4mGIViSOv2EspA'));
   });
 
   it('prints the address of the private key', () => {
