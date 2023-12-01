e50294a77d3031a882dfbee34b7a851ffee6a944
Lorenzo Sicilia <lorenzo.sicilia@outlierventures.io>
2023-12-01 14:11:51 +0000

    fix: use CLI commands from the GitHub action wrapper

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 import { getInput, setOutput, setFailed } from '@actions/core'
-import {setRecord} from './src/setRecord.js'
-import { upload } from './src/upload.js'
-import { getAddress } from './src/utils/info.js'
-import {parsePrivateKey} from './src/utils/parsePrivateKey.js'
+import uploadCommand from './src/commands/uploadCommand.js'
+import infoCommand from './src/commands/infoCommand.js'
+import setCommand from './src/commands/setCommand.js'
 
 try {
   const command = getInput('command')
   const directory = getInput('directory')
-  const dryRun = getInput('dry-run')
-  const base64PrivateKey = getInput('private-key')
+  const dryRun = getInput('dry-run').toLowerCase() === 'true'
+  const privateKey = getInput('private-key')
   const manifestId = getInput('manifest-id')
   const antAddress = getInput('ant-address')
 
-  const privateKey = parsePrivateKey(base64PrivateKey)
-
   switch (command) {
     case 'upload':
-      await upload(directory, privateKey, dryRun.toLowerCase() === 'true')
+      await uploadCommand.handler({
+        directory,
+        antAddress,
+        dryRun,
+        privateKey,
+      })
       break
     case 'address':
-      console.log('address:', getAddress(privateKey))
-      setOutput('address', getAddress(privateKey))
+      const address = await infoCommand.handler({
+        privateKey
+      })
+      setOutput('address', address)
       break
     case 'set':
-      await setRecord(antAddress, manifestId, privateKey, dryRun)
+      await setCommand.handler({
+        antAddress,
+        manifestId,
+        privateKey,
+        dryRun
+      })
       break
     default:
       throw new Error(`${command} not supported`)
--- src/commands/infoCommand.js
+++ src/commands/infoCommand.js
@@ -1,3 +1,4 @@
+import {add} from '@semantic-release/git/lib/git.js'
 import { parsePrivateKey } from '../utils/parsePrivateKey.js'
 import { getAddress } from '../utils/info.js'
 
@@ -16,7 +17,7 @@ export const handler = async (argv) => {
   const privateKey = parsePrivateKey(argv.privateKey)
   const address = getAddress(privateKey)
   console.log(address)
-  return true
+  return address
 }
 
 export default {
